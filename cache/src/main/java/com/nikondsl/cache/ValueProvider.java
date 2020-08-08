bb30f3606626ca02e73fa2e7cbab7fae3ed521bb
igor.nikonoff <igor.nikonoff@gmail.com>
2020-08-08 22:41:32 +0300

    initial commit

--- cache/src/main/java/com/nikondsl/cache/ValueProvider.java
+++ cache/src/main/java/com/nikondsl/cache/ValueProvider.java
@@ -2,9 +2,25 @@ package com.nikondsl.cache;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Factory for providing calculation for values by given keys.
+ * @param <K>
+ * @param <V>
+ * @param <E>
+ */
 public interface ValueProvider<K, V, E extends Throwable> {
+	/**
+	 * This factory should return created element which is associated with given key.
+	 * @param key
+	 * @return constructed value for given key.
+	 * @throws Exception if value cannot be created.
+	 */
 	V createValue(K key) throws E;
 	
+	/**
+	 * TimeTToLive newly created element in cache.
+	 * @return milliseconds till element in cache is not expired and may be used.
+	 */
 	default long getTimeToLive() {
 		return TimeUnit.SECONDS.toMillis(1L);
 	}
