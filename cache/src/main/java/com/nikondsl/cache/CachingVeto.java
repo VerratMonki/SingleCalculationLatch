b78b9da600ea7152e89e9d3a627b481c4701960b
igor.nikonoff <igor.nikonoff@gmail.com>
2020-08-08 22:35:53 +0300

    initial commit

--- cache/src/main/java/com/nikondsl/cache/CachingVeto.java
+++ cache/src/main/java/com/nikondsl/cache/CachingVeto.java
@@ -1,16 +1,38 @@
 package com.nikondsl.cache;
 
+/**
+ * Provides some permissions/rules for caching.
+ * @param <K> class for key in cache.
+ * @param <V> class for value in cache.
+ */
 public interface CachingVeto<K, V> {
 	
-	
+	/**
+	 * Returns if removing from cache enabled.
+	 * @param key
+	 * @param value
+	 * @return true only if this key,value pair is allowed to be removed.
+	 */
 	default boolean removeAllowed(K key, V value) {
 		return true;
 	}
 	
+	/**
+	 * Returns whether element in cache may be expired.
+	 * @param key
+	 * @param value
+	 * @return true only if this key,value pair is allowed to be expired.
+	 */
 	default boolean expireAllowed(K key, V value) {
 		return true;
 	}
 	
+	/**
+	 * Returns whether putting into a cache enabled.
+	 * @param key
+	 * @param value
+	 * @return true only if putting enabled.
+	 */
 	default boolean putInCasheAllowed(K key, V value) {
 		return true;
 	}
